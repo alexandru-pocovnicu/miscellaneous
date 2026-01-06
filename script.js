a9f268120dabb0d1c70f03314ece71396344c7bd
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-06 22:41:51 +0000

    Implement larger function to filter values greater than 1 from an array

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function otherAngle(a, b) {
-  return 180-(a+b)
-}
-console.log(otherAngle(15,25));
+let arr = [1, 2];
 
+function larger(x){
+    let arri=[]
+    for(let i=0;i<x.length;i++){
+        if(x[i]>1){
+            arri.push(x[i])
+        }
+    }
+    return arri
+}
+console.log((larger(arr)));
+console.log(arr.filter((x)=>x>1));
