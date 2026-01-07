a23d82b0ebbba7130c37aa0fe53497c8c1f264ff
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-07 17:57:13 +0000

    Refactor century function to hoopCount for hooping feedback based on count

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function century(year) {
- return Math.ceil(year/100)
+function hoopCount(n) {
+  if(n>=10){
+    return "Great, now move on to tricks";
+  }
+  return "Keep at it until you get it";
 }
-console.log(century(2742));
+console.log(hoopCount(71));
