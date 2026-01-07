8360a3d29f09839833b4d3e7dc5177db5782f156
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-07 22:30:03 +0000

    Refactor betterThanAverage function to calculate if a score is above average

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-String.prototype.isUpperCase = function () {
-  return this===this.toUpperCase()
-};
-console.log();
+function betterThanAverage(classPoints, yourPoints) {
+  const points=classPoints.reduce((acc,curr)=>acc+curr,0)
+  console.log(points);
+  
+  return yourPoints>(points+yourPoints)/(classPoints.length+1)
+}
+console.log(betterThanAverage([2,3],5));
