fb92aa66b307c4dbee0cdf19170a2782ecd9ccf3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-08 21:40:55 +0000

    Refactor rentalCarCost function to implement solution for sorting an array of numbers

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-function rentalCarCost(d) {
-  // let price=0
-  // if(d>=7){
-  //  return price=d*40-50
-  // }else if(d>=3){
-  //   return price=d*40-20
-  // }
-  // return d*40
-  return d>=7 ? d*40-50:d>=3 ? d*40-20:d*40
+function solution(nums) {
+  if(!Array.isArray(nums)){
+    return []
+  }
+  return nums.sort((a,b)=>a-b)
 }
-console.log(rentalCarCost(3));
