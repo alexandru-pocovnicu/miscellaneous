1ace91167c646418419ae6992ec24f788194c5e5
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-08 21:49:48 +0000

    Implement findDifference function to calculate the absolute difference between volumes of two arrays

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function reverseList(list) {
-  return list.reverse()
+function findDifference(a, b) {
+  const volumeA=a.reduce((acc,cur)=>acc*cur,1)
+  const volumeB = b.reduce((acc, cur) => acc * cur, 1);
+  return Math.abs(volumeA-volumeB)
 }
