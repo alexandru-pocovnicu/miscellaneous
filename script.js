4e259c5063d07dcc88778717efef2586a31f7100
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-08 18:38:41 +0000

    Replace calculateYears function with sortArray function to sort characters in a string

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-function calculateYears(principal, interest, tax, desired) {
-  let sum = 0;
-  
-for(i=0;principal<desired;i++){
-  const interestEarned=principal*interest
-  const taxPayed=interestEarned*tax
-  const netInterest=interestEarned-taxPayed
-  let newPrincipal=principal+netInterest
-  principal=newPrincipal
-  sum +=1
-}
-  return sum
-}
-console.log(calculateYears(1000, 0.05, 0.18, 1100));
+let sortArray = function (value) {
+  return value.split("").sort((a,b)=>a-b).join("")
+};
+console.log(sortArray("1964"));
