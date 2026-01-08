bb56e083571bd3993a139d1995a88d23b5f54fc8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-08 20:46:51 +0000

    Refactor rentalCarCost function to simplify rental cost calculation logic

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getAge(inputString) {
-  return Number(inputString.slice(0,1))
+function rentalCarCost(d) {
+  // let price=0
+  // if(d>=7){
+  //  return price=d*40-50
+  // }else if(d>=3){
+  //   return price=d*40-20
+  // }
+  // return d*40
+  return d>=7 ? d*40-50:d>=3 ? d*40-20:d*40
 }
-console.log(typeof(getAge("4 years old")));
+console.log(rentalCarCost(3));
