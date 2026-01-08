b0657d8a227f93acaf146ea75900fea73b19369b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-08 16:12:50 +0000

    Refactor calculateYears function to compute the number of years to reach a desired amount with interest and tax

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function between(a, b) {
-  let arr=[]
-  for(i=a;i<=b;i++){
-    arr.push(i)
-  }
-  return arr
+function calculateYears(principal, interest, tax, desired) {
+  let sum = 0;
+  
+for(i=0;principal<desired;i++){
+  const interestEarned=principal*interest
+  const taxPayed=interestEarned*tax
+  const netInterest=interestEarned-taxPayed
+  let newPrincipal=principal+netInterest
+  principal=newPrincipal
+  sum +=1
 }
-console.log(between(1,4));
-
-
-
+  return sum
+}
+console.log(calculateYears(1000, 0.05, 0.18, 1100));
