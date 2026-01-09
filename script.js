26832a537f3401c28cdbfff39342ee391e8d0510
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-09 17:46:12 +0000

    Replace getGrade function with peopleWithAgeDrink function to determine appropriate drinks based on age

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function getGrade (s1, s2, s3) {
-  const score= (s1+s2+s3)/3
-  return score>=90 && score<=100 ? "A"
-  : score>=80 && score<90 ? "B"
-  : score>=70 && score<80 ? "C"
-  : score>=60 && score<70 ? "D"
-  : score>=0 && score<60 ? "F":null
-
+function peopleWithAgeDrink(old) {
+  return old < 14
+    ? "drink toddy"
+    : old < 18
+    ? "drink coke"
+    : old < 21
+    ? "drink beer"
+    : "drink whisky";
 }
-console.log(getGrade(65,70,59));
+console.log(peopleWithAgeDrink());
