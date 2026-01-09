1b48b7c4e1c8d0cde4a43797f22f2e0e022e1cb7
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-09 10:15:14 +0000

    Replace arithmetic function with squareUp function to calculate the square of a number

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function arithmetic(a, b, operator){
-  return operator==="add" ? a+b 
-  : operator==="subtract" ? a-b 
-  : operator==="multiply" ? a*b : a/b
-  //: operator==="divide" ? a/b 
+function squareUp(x){
+  return x*x
 }
