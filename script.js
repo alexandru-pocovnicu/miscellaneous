c680f83420154b0066d8b97f8c4bc5dab5000263
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-09 17:38:21 +0000

    Implement getGrade function to calculate letter grades based on average scores

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function squareUp(x){
-  return x*x
+function getGrade (s1, s2, s3) {
+  const score= (s1+s2+s3)/3
+  return score>=90 && score<=100 ? "A"
+  : score>=80 && score<90 ? "B"
+  : score>=70 && score<80 ? "C"
+  : score>=60 && score<70 ? "D"
+  : score>=0 && score<60 ? "F":null
+
 }
+console.log(getGrade(65,70,59));
