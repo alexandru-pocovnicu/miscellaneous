45f9f5974a76b618de2c0b2da09da48dca7753d8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-11 16:32:26 +0000

    Refactor sumStr function to convert string inputs to numbers and return their sum as a string

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function saleHotdogs(n) {
-  return n < 5 ? n * 100 : n < 10 ? n * 95 : n * 90;
+function sumStr(a, b) {
+  console.log(typeof Number(a));
+  
+  return (Number(a)+Number(b)).toString()
 }
+console.log(sumStr("4", "5"));
