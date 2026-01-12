11578d114beb7e5b2f71972ddbf0ed5e2422e83b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-12 11:33:02 +0000

    Refactor squareSum function to calculate the sum of squares of an array

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function unusualFive() {
-  let arr=["a","s","d","w"]
-  return arr.length;
+function squareSum(numbers) {
+ //const sqr= numbers.map(x=>x*x)
+  return numbers.map((x) => x * x).reduce((acc, curr) => acc + curr);
 }
+console.log(squareSum([1,2,2]));
