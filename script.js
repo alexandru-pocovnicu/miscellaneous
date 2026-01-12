c33ffbfeffea7f45185526fd8b9af9678900a38b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-12 11:11:55 +0000

    Refactor makeUpperCase function to convert input string to uppercase

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function SeriesSum(n) {
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    sum += 1/(1 + 3 * i);
-  }
-
-  return sum.toFixed(2);
+function makeUpperCase(str) {
+return str.toUpperCase()
 }
-console.log(SeriesSum(3));
