83e7ddb5518d648c053e412c4da71f5025693096
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-12 11:39:01 +0000

    Refactor squareSum function to improve readability with a conditional return

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function squareSum(numbers) {
- //const sqr= numbers.map(x=>x*x)
-  return numbers.map((x) => x * x).reduce((acc, curr) => acc + curr);
+return numbers.length === 0
+  ? 0
+  : numbers.map((x) => x * x).reduce((acc, curr) => acc + curr);
 }
 console.log(squareSum([1,2,2]));
