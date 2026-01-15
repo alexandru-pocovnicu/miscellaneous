a60b075f2955a471ac1eea2c7b689808669d4c69
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-15 12:39:52 +0000

    Refactor printerError function to count characters in the range 'n' to 'z' and return error ratio

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getChar(c) {
-  return String.fromCodePoint(c);
+function printerError(s) {
+ const denominator=s.length
+ let numerator=0
+for( let char of s){
+  if(char >="n" && char<="z"){
+    numerator +=1
+  } 
 }
-console.log(getChar(65)); // Proper Unicode support
+ return `${numerator}/${denominator}`
+}
+console.log(printerError("fffsss"));
