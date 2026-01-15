03bdd8e5615e179cbcbabe4cb926d11e6e406364
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-15 15:36:55 +0000

    Refactor code to implement mouthSize function for determining the size of an animal's mouth

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function getMiddle(s) {
- const firstSubS=s.substring(0,s.length / 2);
- const secondSubS = s.substring((s.length / 2));
-  if(s.length%2===0)return firstSubS[firstSubS.length-1].concat(secondSubS[0]) 
-    return secondSubS[0]  
+function mouthSize(animal) {
+  return animal.toLowerCase() == "alligator" ? "small" : "wide";
 }
-console.log(getMiddle("bdgym"));
