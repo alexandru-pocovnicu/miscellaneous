f6e489b6874061a803881742edda4fd57d4dcf5a
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-17 22:26:08 +0000

    Refactor code to implement twoSort function for sorting an array and formatting the first element

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function onlyOne(...flags) {
-if(flags.length===0)return false
-return (
-  flags.indexOf(true) !== -1 && flags.indexOf(true) === flags.lastIndexOf(true)
-);
+function twoSort(s) {
+  return s.sort()[0].split('').map(element=>element+"***").join("").slice(0,-3)
 }
-console.log(onlyOne([]));
+console.log(
+  twoSort([
+    "take",
+    "over",
+    "the",
+    "world",
+    "maybe",
+    "who",
+    "knows",
+    "perhaps",
+    "bitcoin"
+  ]),
+);
