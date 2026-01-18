1a256e79410d7f7a84e1850b0749b451ac8eef95
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-18 17:51:56 +0000

    Refactor code to implement checkForFactor function for checking if a number is a factor of another

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-function mergeArrays(arr1, arr2) {
-  
-  return [...new Set(arr1.concat(arr2).sort((a, b) => a - b))];
+function checkForFactor(base, factor) {
+  return base % factor === 0;
 }
-console.log(mergeArrays([1, 2, 3, 4], [9, 9, 7, 5]));
