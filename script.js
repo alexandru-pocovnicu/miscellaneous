2ded1d66dd79dc93f3b7f26614cee96c726ad174
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-18 21:42:23 +0000

    Write a function which calculates the average of the numbers in a given array.
    Note: Empty arrays should return 0.

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-function add(num1, num2) {
-  let arr1 = num1.toString();
-  let arr2 = num2.toString();
-  const longerLength = Math.max(arr1.length, arr2.length);
-  
- arr1 = arr1.padStart(longerLength, "0");
- arr2 = arr2.padStart(longerLength, "0");
-  let sum=[]
-  for (let i = 0; i < longerLength; i++) {
-    sum.push(+arr1[i] + +arr2[i]);
-  }
-  return sum.join('')
+function findAverage(array) {
+  // your code here
+  return array.length===0 ? 0 : array.reduce((acc,cur)=>acc+cur)/array.length
 }
-console.log(add(2, 11));
