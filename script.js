c9bb55858b621bbef9707b677b192d16c9b34b3a
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-19 12:49:28 +0000

    Refactor replace function to abbrevName for generating initials from a full name

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function replace(s) {
-  //coding and coding....
-   return s.replaceAll(/[aeiouAEIOU]/g, "!");
+function abbrevName(name) {
+  return name.split(" ").map(element=>element[0]+element[element.length-1]);
 }
+console.log(abbrevName("Sam Harris"));
