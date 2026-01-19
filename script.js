76a5620c505513b412cb252216d6eeee73a8b068
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-19 20:46:39 +0000

    Enhance arrayPlusArray function to filter out NaN and non-numeric values before summation

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function arrayPlusArray(arr1, arr2) {
-  return arr1.concat(arr2).reduce((a,s)=>a+s); //something went wrong
+  return arr1
+    .concat(arr2)
+    .filter((x) => !Number.isNaN(x))
+    .filter((x) => +x)
+    .reduce((a, s) => a + s); 
 }
-console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
+console.log(arrayPlusArray([1, 2, 3], [4, 5,NaN,6,"k",[1,2]]));
