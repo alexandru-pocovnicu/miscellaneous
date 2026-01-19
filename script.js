734ee93997f798a4f545cfa7b5d11b504cb9143e
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-19 16:20:24 +0000

    Refactor gordon function to implement past function for calculating total milliseconds from hours, minutes, and seconds

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-function gordon(a) {
-  
-  return a.split(" ").map(
-    (element) =>
-      element
-        .toUpperCase()
-        .replaceAll(/[aA]/g, "@")
-        .replaceAll(/[eiouEIOU]/g, "*") + "!!!!").join(" ");
+function past(h, m, s) {
+const seconds=s*1000
+const minutes=m*60*1000
+const hours=h*3600*1000
+return hours+minutes+seconds
 }
-console.log(gordon("What feck damn cake"));
+console.log(past(0,1,1));
