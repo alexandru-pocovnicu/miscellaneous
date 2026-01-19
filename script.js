4db3bddee9789e86f977c8a4ae245821afc6bbc5
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-19 19:02:15 +0000

    Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function past(h, m, s) {
-const seconds=s*1000
-const minutes=m*60*1000
-const hours=h*3600*1000
-return hours+minutes+seconds
+function divisibleBy(numbers, divisor) {
+  let arr=[]
+  for(let element of numbers){
+    if(element%divisor===0){
+      arr.push(element)
+    }
+  }
+  return arr
 }
-console.log(past(0,1,1));
