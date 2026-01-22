ba4d7566ecd6a03aceac1ebc242b48cb30c6a875
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-22 14:08:18 +0000

    You need to write a function that reverses the words in a given string. Words are always separated by a single space.
    As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-var humanYearsCatYearsDogYears = function (humanYears) {
-  let all = [];
-  let cat = 0;
-  let dog = 0;
-  if (humanYears > 2) {
-    cat = (humanYears - 2) * 4 + 24;
-    dog = (humanYears - 2) * 5 + 24;
-    all.push(humanYears, cat, dog);
-  }
-  if (humanYears === 2) {
-    cat = 24;
-    dog = 24;
-    all.push(humanYears, cat, dog);
-  }
-  if (humanYears === 1) {
-    cat = humanYears * 15;
-    dog = humanYears * 15;
-    all.push(humanYears, cat, dog);
-  }
-  return all;
-};
+function reverse(string) {
+  return string.split(" ").reverse().join(" ");
+}
