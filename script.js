1551945a2618ed86e4cf718747cf3b2191efbd86
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-22 13:44:43 +0000

    Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
    Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-function arrayPlusArray(arr1, arr2) {
-  return arr1
-    .concat(arr2)
-    .filter((x) => !Number.isNaN(x))
-    .filter((x) => +x)
-    .reduce((a, s) => a + s); 
+function hello(name) {
+  return !name
+    ? "Hello, World!"
+    : `Hello, ${name[0].toUpperCase().concat(name.substring(1).toLowerCase())}!`;
 }
-console.log(arrayPlusArray([1, 2, 3], [4, 5,NaN,6,"k",[1,2]]));
