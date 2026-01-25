4ac15cc64cd48cf4205f786b8ded80e747587014
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-25 17:20:37 +0000

    if a bottle costs £12 normally and the duty free discount is 50%, you would save £6 per bottle. If your holiday will cost £1000, you would have to purchase 166.66 bottles to save £1000, so your answer should be 166 bottles.

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function pipeFix(numbers) {
-  let arr=[]
-  for(let i=numbers[0];i<numbers[numbers.length-1];i++){
-    arr.push(i+1)
-  }
-  let newArray=[]
-  newArray[0] = numbers[0];
-  return  newArray.concat(arr)
+function dutyFree(normPrice, discount, hol) {
+  return Math.floor(
+    hol / (normPrice - (normPrice - (discount / 100) * normPrice)),
+  );
 }
-console.log(pipeFix([1,2,3,5,6,8,9]))
