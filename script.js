6eae2aa726ac9b515c5cc4d14c307a41f47d23af
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-26 20:29:51 +0000

    Given an array add all the number elements and return the binary equivalent of that sum; to make the code bullet proof anything else than a number should be added as 0 since it can't be added.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function polygonArea(A, B, C, D) {
-  const rectangleArea = A * B;
+function arr2bin(arr) {
+  
+// if(arr.length===0) return "0"
+// if (arr.includes(NaN)) return NaN;
+//const filteredArr=arr.filter(x=>typeof x==="number")
+return arr.length === 0
+  ? "0"
+  : arr.includes(NaN)
+    ? NaN
+    : arr.filter((x) => typeof x === "number").length === 0
+      ? "0"
+      : arr
+          .filter((x) => typeof x === "number")
+          .reduce((x, y) => x + y)
+          .toString(2);
 
-  const triangleArea = (1 / 2) * B * (C - A);
-  return triangleArea + rectangleArea;
 }
-console.log(polygonArea(2, 5, 10, 2.5));
+console.log(arr2bin([1,2,3,4,5,{}]));
