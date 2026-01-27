0c184fb2e6f8974bce8875166adacc1674c18ee8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-27 20:21:31 +0000

    Your task is to find the first element of an array that is not consecutive.
    By not consecutive we mean not exactly 1 larger than the previous element of the array.
    
    E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
    
    If the whole array is consecutive then return null2.
    
    The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function howManyLightsabersDoYouOwn(name) {
-  return name === "Zach" ? 18 : 0;
+function firstNonConsecutive(arr) {
+    if(arr.length<2) return null
+    let num=0
+    for(let i=1;i<arr.length;i++){
+        if(arr[i]!==arr[i-1]+1){
+           return num=arr[i]
+        }
+    }
+    return null
 }
+console.log(firstNonConsecutive([1,2,3,4,6,7,8]));
