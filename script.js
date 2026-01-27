d896a000888e377d8fd10930a77e8784110a2ea8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-27 19:56:03 +0000

    Inspired by the development team at Vooza, write the function that
    accepts the name of a programmer, and
    returns the number of lightsabers owned by that person.
    The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-function arr2bin(arr) {
-  
-// if(arr.length===0) return "0"
-// if (arr.includes(NaN)) return NaN;
-//const filteredArr=arr.filter(x=>typeof x==="number")
-return arr.length === 0
-  ? "0"
-  : arr.includes(NaN)
-    ? NaN
-    : arr.filter((x) => typeof x === "number").length === 0
-      ? "0"
-      : arr
-          .filter((x) => typeof x === "number")
-          .reduce((x, y) => x + y)
-          .toString(2);
-
+function howManyLightsabersDoYouOwn(name) {
+  return name === "Zach" ? 18 : 0;
 }
-console.log(arr2bin([1,2,3,4,5,{}]));
