0d729fed2fc2b1b619e949490008d31dd3657f94
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-29 19:28:56 +0000

    return largest number

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function fakeBin(x) {
-   let xArr=x.split("")
-   let arr=[]
+// function fakeBin(x) {
+//    let xArr=x.split("")
+//    let arr=[]
   
-   for(let i of xArr){}
-    if(i<5){
-      xArr.splice(i,1,"0")
-      if (i > 5) {
-        xArr.splice(i, 1, "1");
-      }
-    }
-    return xArr
+//    for(let i of xArr){}
+//     if(i<5){
+//       xArr.splice(i,1,"0")
+//       if (i > 5) {
+//         xArr.splice(i, 1, "1");
+//       }
+//     }
+//     return xArr
+// }
+// console.log(fakeBin("12345678"));
+function largestNum(num1,num2,num3){
+  return Math.max(num1,num2,num3)
 }
-console.log(fakeBin("12345678"));
+console.log(largestNum(1,7,3));
