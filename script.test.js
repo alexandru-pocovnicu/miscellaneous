16946032240333497c688655362600bf87bec077
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-30 10:19:06 +0000

    Add test case for isEven function to check for non-numerical values

--- script.test.js
+++ script.test.js
@@ -7,3 +7,7 @@ test("isEven should return true for even numbers", ()=> {
 test("isEven should return false for odd numbers", () => {
   expect(isEven(3)).toBe(false);
 });
+test("isEven should return NaN for a non numerical value", () => {
+    let result=isEven("h")
+  expect(Number.isNaN(result)).toBe(false);
+});
