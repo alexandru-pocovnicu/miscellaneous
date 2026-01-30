7300ce1218cb201ad8d3c5db6369ce44ad5b9aff
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-01-30 09:56:26 +0000

    Add test case for isEven function to check for odd numbers

--- script.test.js
+++ script.test.js
@@ -4,3 +4,6 @@ function isEven(n){
 test("isEven should return true for even numbers", ()=> {
     expect(isEven(2)).toBe(true)
 })
+test("isEven should return false for odd numbers", () => {
+  expect(isEven(3)).toBe(false);
+});
