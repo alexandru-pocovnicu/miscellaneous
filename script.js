bf344198fbe7e3d57e7f65d7b792f606614fc598
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-02 18:58:06 +0000

    Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// function fakeBin(x) {
-//    let xArr=x.split("")
-//    let arr=[]
-  
-//    for(let i of xArr){}
-//     if(i<5){
-//       xArr.splice(i,1,"0")
-//       if (i > 5) {
-//         xArr.splice(i, 1, "1");
-//       }
-//     }
-//     return xArr
-// }
-// console.log(fakeBin("12345678"));
-function largestNum(num1,num2,num3){
-  return Math.max(num1,num2,num3)
+function take(arr, n) {
+  return arr.slice(0,n)
 }
-console.log(largestNum(1,7,3));
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
