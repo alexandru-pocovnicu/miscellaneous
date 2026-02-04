2002229d60e03788a4cbdc8b5d2d1da173f1f0a0
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-04 20:54:37 +0000

    In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
    make as few changes as possible.
    if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function whatday(num) {
-  return num === 1
-    ? "Sunday"
-    : num === 2
-      ? "Monday"
-      : num === 3
-        ? "Tuesday"
-        : num === 4
-          ? "Wednesday"
-          : num === 5
-            ? "Thursday"
-            : num === 6
-              ? "Friday"
-              : num === 7
-                ? "Saturday"
-                : "Wrong, please enter a number between 1 and 7";
+function solve(s) {
+  let upper = 0;
+  let lower = 0;
+  const arrS = s.split("");
+  for (let elem of arrS) {
+    if (elem === elem.toUpperCase()) {
+      upper += 1;
+    } else {
+      lower += 1;
+    }
+  }
+  return upper > lower ? s.toUpperCase() : s.toLowerCase();
 }
+console.log(solve("COdE"));
