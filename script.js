b0f64f3838cf1a157b66c28aee423674d07ad9d7
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-08 10:35:45 +0000

    Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function rowWeights(array) {
-   
-  let team1 = [];
-  let team2 = [];
-  for (let index in array) {
-    if (index % 2 === 0) {
-      team1.push(array[index]);
-    } else {
-      team2.push(array[index]);
-    }
-  }
-
-  const team1Sum= team1.reduce((a,b)=>a+b) 
-  const team2Sum=team2.length>0 ? team2.reduce((a,b)=>a+b):0
-  return [team1Sum,team2Sum]
+function include(arr, item) {
+  return arr.includes(item);
 }
-console.log(rowWeights([50, 60, 70, 80]));
