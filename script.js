7a4c4d138fc78c8de0edf10a239974148f732c29
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-08 09:20:32 +0000

    Given an array of positive integers (the weights of the people), return a new array / tuple of two integers (depending on your language), whereby the first one is the total weight of team 1, and the second one is the total weight of team 2. Note that the array will never be empty.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function animal(obj) {
-  return `This ${obj.color} ${obj.name} has ${obj.legs} legs `
+function rowWeights(array) {
+   
+  let team1 = [];
+  let team2 = [];
+  for (let index in array) {
+    if (index % 2 === 0) {
+      team1.push(array[index]);
+    } else {
+      team2.push(array[index]);
+    }
+  }
+
+  const team1Sum= team1.reduce((a,b)=>a+b) 
+  const team2Sum=team2.length>0 ? team2.reduce((a,b)=>a+b):0
+  return [team1Sum,team2Sum]
 }
+console.log(rowWeights([50, 60, 70, 80]));
