712d7785d75c7c256b20018eb62ea3f1ab3c66a9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-08 19:50:48 +0000

    Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
    The input will always be made of only lowercase letters and will never be empty.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function spEng(sentence) {
-  return sentence.toLowerCase().includes("english");
+function wordsToMarks(string) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let sum=0
+  for(let elem of string){
+    sum +=alphabet.indexOf(elem)+1
+  }
+  return sum
 }
+console.log(wordsToMarks("z"));
