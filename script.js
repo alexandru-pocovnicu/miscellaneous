4803ca677a978f3a3c507f2be0cba704500db035
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-10 13:55:06 +0000

    Remove all exclamation marks from the end of sentence.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function calculateAge(yb, yc) {
-  const years = Math.abs(yb - yc);
-  if (yc - yb > 1) return `You are ${yc - yb} years old.`;
-  if (yc - yb === 1) return `You are ${yc - yb} year old.`;
-  if (yb - yc > 1) return `You will be born in ${yb - yc} years.`;
-  if (yb - yc === 1) return `You will be born in ${yb - yc} year.`;
-  return "You were born this very year!";
+function remove(string) {
+  let arr = string.split("");
+  while (arr[arr.length - 1] === "!") {
+    arr.pop();
+  }
+  return arr.join("");
 }
+console.log(remove("hi!hi!!!!"));
