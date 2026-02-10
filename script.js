3e1f7a7ada5e9b68dac0c363d269bcfe57f67b6c
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-10 15:03:30 +0000

    Pig Latin is an English language game where the goal is to hide the meaning of a word from people not aware of the rules.
    So, the goal of this kata is to wite a function that encodes a single word string to pig latin.
    
    The rules themselves are rather easy:
    
    The word starts with a vowel(a,e,i,o,u) -> return the original string plus "way".
    
    The word starts with a consonant -> move consonants from the beginning of the word to the end of the word until the first vowel, then return it plus "ay".
    
    The result must be lowercase, regardless of the case of the input. If the input string has any non-alpha characters, the function must return None, null, Nothing (depending on the language).
    
    The function must also handle simple random strings and not just English words.
    
    The input string has no vowels -> return the original string plus "ay".

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function descendingOrder(n) {
-  return +([...String(n)].sort((a, b) => b - a)).join("");
+function pigLatin(string) {
+  if (![...string].every((x) => x.match(/[a-zA-Z]/))) return null;
+  if(!/[aeiou]/i.test(string))return string.toLowerCase() + "ay";
+  while(string[0].match(/[aeiou]/i)){
+    return string.toLowerCase()+"way"
+  }
+  
+  while (!(/[aeiou]/i.test(string[0]))) {
+    string=string.slice(1)+string[0]
+
+    
+  }
+  return string.toLowerCase() + "ay"
 }
-console.log(descendingOrder(48538));
+console.log(pigLatin("r2"));
