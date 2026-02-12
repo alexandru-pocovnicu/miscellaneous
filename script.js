20255bbfbe6f8f3b68019b6a8c726a2ac4bf1be7
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-12 13:52:42 +0000

    This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
    Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function pigLatin(string) {
-  if (![...string].every((x) => x.match(/[a-zA-Z]/))) return null;
-  if(!/[aeiou]/i.test(string))return string.toLowerCase() + "ay";
-  while(string[0].match(/[aeiou]/i)){
-    return string.toLowerCase()+"way"
-  }
-  
-  while (!(/[aeiou]/i.test(string[0]))) {
-    string=string.slice(1)+string[0]
-
-    
-  }
-  return string.toLowerCase() + "ay"
+function derive(coefficient, exponent) {
+  let multiplication=coefficient*exponent
+  let substraction=exponent-1
+  return `${multiplication}x^${substraction}`
 }
-console.log(pigLatin("r2"));
