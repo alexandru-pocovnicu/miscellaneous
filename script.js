bd29ef0218e847fdebaa3f9392cf7b118c0db5da
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-15 09:09:42 +0000

    Given a random non-negative number, you have to return the digits of this number within an array in reverse order.

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function sameCase(a, b) {
-  let charA=/^[a-zA-Z]$/.test(a) 
-  let charB = /^[a-zA-Z]$/.test(b); 
-  if(charA===false || charB===false){
-    return -1
-  }
-  let caseA=a===a.toUpperCase()
-  let caseB = b === b.toUpperCase();
-  return caseA===caseB ? 1:0;
+function digitize(n) {
+  return [...String(n)].reverse().map(x=>+x)
 }
-console.log(sameCase("d",1));
+console.log(digitize(35231));
