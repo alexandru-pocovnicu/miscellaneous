6b7c2c3542e437f35c2cf442ce76fa892422e614
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-16 16:23:07 +0000

    Add greet function with parameter shadowing global variable

---
+++ scope.js
@@ -0,0 +1,25 @@
+// const name = "Jill"; //variable declared globally
+// function greet(){
+//     console.log(name)
+// }
+// greet()
+
+
+// const name="Jill" //global variable
+
+// function greet(){
+//     const name="Kim" //local variable
+//     console.log(name);
+    
+// }
+// greet()
+
+
+const name ="Jill" //global variable
+
+function greet(name){  //parameter with the same name as the global variable
+    console.log(name);
+    
+}
+
+greet("Michael")
