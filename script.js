8b17ff53e0bdd759f22971656c2851d864fc92d4
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-17 13:06:19 +0000

    Define a method/function that removes from a given array of integers all the values contained in a second array.

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-function gimme(triplet) {
-  let sortedArray=[...triplet].sort((a,b)=>a-b)
-  let middle=sortedArray[1]
-  return getIndex(triplet,middle)
-}
-
-
-function getIndex(array,element){
-  let indexPosition=0
-for(let index in array){
-  if(array[index]===element){
-    indexPosition=index
+Array.prototype.remove_ = function (integer_list, values_list)  {
+  while(listContainsValue(integer_list, values_list)){
+    removeElement(integer_list,values_list)
   }
+  return integer_list
+};
+
+
+console.log([].remove_([1, 1, 2, 3, 1, 2, 3, 4], [5, 3]));
+
+
+function listContainsValue(array1,array2){
+  for(let element of array2){
+    if(array1.includes(element)){
+      return true
+    }
+  }
+  return false
 }
-return +indexPosition
+
+
+function removeElement(array1,array2){
+
+  for(let element of array2){
+    while(array1.includes(element)){
+      array1.splice(array1.indexOf(element),1)
+    }
+  }
+ 
+  return array1
 }
 
