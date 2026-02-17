6b6f0e883caf5da005f2c5cc603cdf2cd7020c63
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-17 15:14:49 +0000

    Rick wants a faster way to get the product of the largest pair in an array. Your task is to create a performant solution to find the product of the largest two integers in an array of positive numbers.
    Rick is only interested in solutions that are faster than his, which has a running time of O(n log n)

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-Array.prototype.remove_ = function (integer_list, values_list)  {
-  while(listContainsValue(integer_list, values_list)){
-    removeElement(integer_list,values_list)
-  }
-  return integer_list
-};
-
-
-console.log([].remove_([1, 1, 2, 3, 1, 2, 3, 4], [5, 3]));
-
-
-function listContainsValue(array1,array2){
-  for(let element of array2){
-    if(array1.includes(element)){
-      return true
-    }
-  }
-  return false
+function maxProduct(a) {
+  return a.splice(a.indexOf(Math.max(...a)),1)*Math.max(...a)
 }
-
-
-function removeElement(array1,array2){
-
-  for(let element of array2){
-    while(array1.includes(element)){
-      array1.splice(array1.indexOf(element),1)
-    }
-  }
- 
-  return array1
-}
-
+console.log(maxProduct([2, 6, 3]));
