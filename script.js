ce45735564cb891c4552bfccb22e751713a3126c
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-21 21:54:51 +0000

    Challenge:
    Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
    
    Example:
    
    Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function processArray(arr, callback) {
-  return callback(arr)
+function flattenAndSort(array) {
+  // Good luck, brave code warrior!
+  return array.flat().sort((a,b)=>a-b)
 }
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]));
