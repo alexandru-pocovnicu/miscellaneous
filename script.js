12adbe1f8b0ce9a23d0d1936752a3ef973a9509b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-21 08:58:38 +0000

    Write the processArray function, which takes an array and a callback function as parameters. The callback function can be, for example, a mathematical function that will be applied on each element of this array. Optionally, also write tests similar to the examples below.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-function uniqueSum(lst) {
-  if(lst.length===0)return null
- return [...new Set(lst)].reduce((a,b)=>a+b)
+function processArray(arr, callback) {
+  return callback(arr)
 }
-console.log(uniqueSum([-1, -1, 5, 2, -7]));
