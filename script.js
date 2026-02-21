68216de87ca700b5c82c188c3caf9fc55ca804ef
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-21 08:42:11 +0000

    Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times in the list, you can only count it once in your sum.

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-function getSocks(name, socks) {
- if(name==="Punky"){
-  return punkySocks(socks)
- }
- return henrySocks(socks)
+function uniqueSum(lst) {
+  if(lst.length===0)return null
+ return [...new Set(lst)].reduce((a,b)=>a+b)
 }
-console.log(getSocks("Punky", ["pink", "pink" ,"pink"]));
-
-function punkySocks(socks){
-  let arr=[]
-for(let i=0;i<socks.length;i++){
-  if(socks[i]!==socks[i+1]){
-    if(arr.length<2){
-    arr.push(socks[i],socks[i+1])
-    }
-  }else if(socks.every(x=>x===socks[i])){
-    return arr
-  }
-}
-return arr
-}
-
-function henrySocks(socks){
-  let arr=[]
-  for(let elem of socks){
-    if(socks.indexOf(elem)!==socks.lastIndexOf(elem)){
-      if(arr.length<2){
-      arr.push(socks[socks.indexOf(elem)]);}
-      }
-  }
-  return arr
-}
-//console.log(henrySocks(["red", "blue","yellow", "blue", "green","blue"]));
+console.log(uniqueSum([-1, -1, 5, 2, -7]));
