1e53f6e5dfc201e88f7441524e6ae75add3fcac5
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-22 10:58:38 +0000

    write a function that when executed as findOddNames(list1) returns only the developers where if you add the ASCII representation of all characters in their first names, the result will be an odd number:

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-function toNumberArray(stringarray) {
-  return stringarray.map(x=>+x)
+function findOddNames(list) {
+  let name = "";
+  let arrOdd = [];
+  for (let i = 0; i < list.length; i++) {
+    name = list[i].firstName;
+    console.log(name);
+
+    if (calculateAscii(name) % 2 === 1) {
+      arrOdd.push(list[i]);
+      console.log(arrOdd);
+    }
+  }
+  return arrOdd;
 }
+console.log(
+  findOddNames([
+    {
+      firstName: "Aba",
+      lastName: "N.",
+      country: "Ghana",
+      continent: "Africa",
+      age: 21,
+      language: "Python",
+    },
+    {
+      firstName: "Aba",
+      lastName: "O.",
+      country: "Israel",
+      continent: "Asia",
+      age: 39,
+      language: "Java",
+    },
+  ]),
+);
+
+function calculateAscii(name) {
+  let sum = 0;
+  for (let i = 0; i < name.length; i++) {
+    sum += name.charCodeAt(i);
+  }
+  return sum;
+}
+
+console.log(calculateAscii("Abb"));
