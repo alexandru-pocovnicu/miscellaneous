ffcf0dc6e7b0106ea8621b519a5914bf21005b66
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-02 17:52:01 +0000

    Implement a function that computes the difference between two lists. The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). The order of elements in the first list should be preserved in the result.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toNumberArray(stringarray) {
-  return stringarray.map(x=>+x)
+function arrayDiff(a, b) {
+  return a.filter(x=>!b.includes(x))
 }
-console.log(toNumberArray(["1", "2", "3"]));
+console.log(arrayDiff([1,2,3],[2,3]));
