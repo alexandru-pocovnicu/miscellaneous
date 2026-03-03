17c993d98d6af3a2024591ebff4bd9e71216e261
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-03 09:08:00 +0000

    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
    
    Additionally, if the number is negative, return 0.
    
    Note: If a number is a multiple of both 3 and 5, only count it once.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function arrayDiff(a, b) {
-  return a.filter(x=>!b.includes(x))
+function solution(number) {
+  if (number<=0)return 0
+  let sum=new Set()
+  for(let i=number-1;i>=0;i--){
+    if(i%3===0 || i%5===0){
+      sum.add(i)
+    }
+  }
+  return [...sum].reduce((a,b)=>a+b)
 }
-console.log(arrayDiff([1,2,3],[2,3]));
+console.log(solution(0));
+
+
+//
