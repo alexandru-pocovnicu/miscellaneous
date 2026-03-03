db3e51864edbe1e59795a72cec3e79d5bb6227d9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-03 11:06:27 +0000

    Write a function called that takes 3 values. The first and third values are numbers. The second value is a character.
    If the character is "+" , "-", "*", or "/", the function will return the result of the corresponding mathematical function on the two numbers.
    If the string is not one of the specified characters, the function should return null (throw an ArgumentException in C#).
    Keep in mind, you cannot divide by zero. If an attempt to divide by zero is made, return null / None (Python) / throw an ArgumentException (C#).

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-function solution(number) {
-  if (number<=0)return 0
-  let sum=new Set()
-  for(let i=number-1;i>=0;i--){
-    if(i%3===0 || i%5===0){
-      sum.add(i)
-    }
-  }
-  return [...sum].reduce((a,b)=>a+b)
+function calculate(a, operator, b) {
+  let operators=["+" , "-", "*",  "/"]
+  if(!operators.includes(operator)|| (operator==="/" && b===0))return null
+  
+    if(operator==="+")return a+b
+    if(operator==="-")return a-b
+    if(operator==="*")return a*b
+  
+  return a/b
 }
-console.log(solution(0));
-
-
-//
+console.log(calculate(2, "*", 2));
