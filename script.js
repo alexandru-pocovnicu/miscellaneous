729e67d658cfd4dbbf3153d7663a3e9769e771d0
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-13 13:47:36 +0000

    Give you an array arr that contains some number elements, find out the peaks and valleys in the array, and return them.
    What do the peaks and valleys mean? If an element is larger than the three one on the left and larger than the three one on the right, we call it a peak. In contrast, an element is smaller than the value of the three elements(left and right), we call it a valley.

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
-function distancesFromAverage(arr) {
-  let average = findAverage(arr);
-  return differenceFromAverage(arr, average).map((a) => Number(a.toFixed(2)));
+function peakAndValley(arr) {
+  let newArr = [];
+ 
+ for(let i=3;i<=arr.length-4;i++){
+  if (
+    arr[i] > arr[i - 1] &&
+    arr[i] > arr[i - 2] &&
+    arr[i] > arr[i - 3] &&
+    arr[i] > arr[i + 1] &&
+    arr[i] > arr[i + 2] &&
+    arr[i] > arr[i +3]
+  ){
+    newArr.push(arr[i])
+  }
+  else if (
+    arr[i] < arr[i - 1] &&
+    arr[i] < arr[i - 2] &&
+    arr[i] < arr[i - 3] &&
+    arr[i] < arr[i + 1] &&
+    arr[i] < arr[i + 2] &&
+    arr[i] < arr[i + 3]
+  ) {
+    newArr.push(arr[i]);
+  }
 }
+    return newArr;
+ }
 
-function findAverage(arr) {
-  return arr.reduce((a, b) => a + b) / arr.length;
-}
+console.log(
+  peakAndValley([
+    10, 20, 30, 40, 30, 20, 10, 11, 12, 13, 14, 15, 16, 15, 14, 13,
+  ]),
+);
 
-function differenceFromAverage(arr, average) {
-  return arr.map((a) => average - a);
-}
+
+
+
+
+// function peak(arr) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > arr[i - 1] && arr[i] > arr[i - 2] && arr[i] > arr[i - 3]) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
+// console.log(
+//   peakAndValley([
+//     10, 20, 30, 40, 30, 20, 10, 11, 12, 13, 14, 15, 16, 15, 14, 13,
+//   ]),
+// );
+
+// function valley(arr) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > arr[i + 1] && arr[i] > arr[i + 2] && arr[i] > arr[i + 3]) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
