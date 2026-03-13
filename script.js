930a91eeb99a734c711f46affc24d4948e9073b1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-13 18:59:59 +0000

    Coding in function howManySmaller, function accept 2 parameter:arr and n. arr is a decimal array. n is a decimal.
    The first mission: let all elements in the array keep two decimal places(No need to convert number n).
    
    The second mission: Traversal arr, count the number of elements which are smaller than n and return it.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function firstToLast(str, c) {
-  if(![...str].includes(c))return -1
-  return str.lastIndexOf(c)-str.indexOf(c)
+function howManySmaller(arr, n) {
+  let newArr=arr.map(x=>+x.toFixed(2))
+  let count=0
+  for(let elem of newArr){
+    if(elem<n){
+      count++
+    }
+  }
+  return count
 }
-console.log(
-  firstToLast(
-    "lupapijjneuvrmgtftcmhgvmamqciliuitrcqdacvrwrifzjfzrfezlveu",
-    "y"
-  ),
-);
+console.log(howManySmaller([1.234, 1.235, 1.228],1.24));
