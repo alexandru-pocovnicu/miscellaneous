3e1572315ed5e0378dda0429e786582fdcd9d65b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-13 13:49:49 +0000

    Remove commented-out code and console.log statements from peakAndValley function

--- script.js
+++ script.js
@@ -26,37 +26,9 @@ function peakAndValley(arr) {
     return newArr;
  }
 
-console.log(
-  peakAndValley([
-    10, 20, 30, 40, 30, 20, 10, 11, 12, 13, 14, 15, 16, 15, 14, 13,
-  ]),
-);
 
 
 
 
 
-// function peak(arr) {
-//   let newArr = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > arr[i - 1] && arr[i] > arr[i - 2] && arr[i] > arr[i - 3]) {
-//       newArr.push(arr[i]);
-//     }
-//   }
-//   return newArr;
-// }
-// console.log(
-//   peakAndValley([
-//     10, 20, 30, 40, 30, 20, 10, 11, 12, 13, 14, 15, 16, 15, 14, 13,
-//   ]),
-// );
 
-// function valley(arr) {
-//   let newArr = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > arr[i + 1] && arr[i] > arr[i + 2] && arr[i] > arr[i + 3]) {
-//       newArr.push(arr[i]);
-//     }
-//   }
-//   return newArr;
-// }
