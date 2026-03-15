cda2fa3badf8d63c5f560719414fe9b09911133f
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-15 22:05:51 +0000

    You are the Dungeon Master for a public DnD game at your local comic shop and recently you've had some trouble keeping your players' info neat and organized so you've decided to write a bit of code to help keep them sorted!
    The goal of this code is to create an array of objects that stores a player's name and contact number from a given string.
    
    The method should return an empty array if the argument passed is an empty string or nil/None/null.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function yourFutureCareer() {
-    let career=Math.random()
-  if (career <= 0.32) {
-    return "FrontEnd Developer";
-  } else if (career <= 0.65) {
-    return "BackEnd Developer";
-  } else {
-    return "Full-Stack Developer";
+function playerManager(players) {
+  if (players === null || players.length === 0) return [];
+  let arr1 = players.split(",");
+  let arrOfObj = [];
+  let arr = arr1.map((x) => x.trim());
+
+  for (let i = 0; i < arr.length; i++) {
+    let obj = {};
+    if (Number.isNaN(parseInt(arr[i]))) {
+      obj.player = arr[i];
+      obj.contact = +arr[i + 1];
+      arrOfObj.push(obj);
+    }
   }
+  return arrOfObj;
 }
-console.log(yourFutureCareer(-20));
+console.log(playerManager("John Doe, 8167238327, Jane Doe, 8163723827"));
