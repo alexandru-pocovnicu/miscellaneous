09de09039a3bc35791a125453105384e2b3c67b3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-16 16:12:35 +0000

    Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ... z = 26.
    You will be given a list of strings and your task will be to return the values of the strings as explained above multiplied by the position of that string in the list. For our purpose, position begins with 1.
    
    ["abc", "abc abc"] should return [6, 24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
    
    "abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied by 1 while the value at position 2 is multiplied by 2.
    
    Input will only contain lowercase characters and spaces.
    
    Good luck!

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-function breachAttempts(hackers, securityLevel, increase) {
-  let sum = 0;
-  let level = securityLevel;
-  for (let i = 0; i < hackers.length; i++) {
-    if (hackers[i] > level) {
-      sum += 1;
-    } else if (hackers[i] <= level) {
-      level += increase;
+function wordValue(words) {
+    let newArr=[]
+    for(let i=0;i<words.length;i++){
+        console.log(words.indexOf(words[i]));
+        
+       return newArr = pushToArray(words).map((val,idx) => val * (idx+ 1),
+        );
     }
+    
+}
+
+
+function sumUp(word) {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let sum = 0;
+  for (let i = 0; i < word.length; i++) {
+    sum += alphabet.indexOf(word[i]) + 1;
   }
   return sum;
 }
-console.log(breachAttempts([7, 6, 8, 9], 6, 2));
+
+function pushToArray(words) {
+  let noSpace = words.map((x) => x.replaceAll(" ", ""));
+  let newArr = [];
+  for (let i = 0; i < noSpace.length; i++) {
+    newArr=(noSpace.map((x) => sumUp(x) ));
+  }
+
+  return newArr;
+}
