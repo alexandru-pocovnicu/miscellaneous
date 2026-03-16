bc3225315f4001cdd120e1dd6c991ebef6ce1e32
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-16 11:29:03 +0000

    count how many times the hacker breches the system

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function playerManager(players) {
-  if (players === null || players.length === 0) return [];
-  let arr1 = players.split(",");
-  let arrOfObj = [];
-  let arr = arr1.map((x) => x.trim());
-
-  for (let i = 0; i < arr.length; i++) {
-    let obj = {};
-    if (Number.isNaN(parseInt(arr[i]))) {
-      obj.player = arr[i];
-      obj.contact = +arr[i + 1];
-      arrOfObj.push(obj);
+function breachAttempts(hackers, securityLevel, increase) {
+  let sum = 0;
+  let level = securityLevel;
+  for (let i = 0; i < hackers.length; i++) {
+    if (hackers[i] > level) {
+      sum += 1;
+    } else if (hackers[i] <= level) {
+      level += increase;
     }
   }
-  return arrOfObj;
+  return sum;
 }
-console.log(playerManager("John Doe, 8167238327, Jane Doe, 8163723827"));
+console.log(breachAttempts([7, 6, 8, 9], 6, 2));
