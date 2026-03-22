411f7366fd2aad4fb34e3220d201d4e3672e31ca
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-22 19:23:17 +0000

    created elements for film card title and director

--- script.js
+++ script.js
@@ -1,57 +1,16 @@
-function wordValue(words) {
-    let newArr=[]
-    for(let i=0;i<words.length;i++){
-        console.log(words.indexOf(words[i]));
-        
-       return newArr = pushToArray(words).map((val,idx) => val * (idx+ 1),
-        );
-    }
-    
-}
-
-
-function sumUp(word) {
-  const alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  let sum = 0;
-  for (let i = 0; i < word.length; i++) {
-    sum += alphabet.indexOf(word[i]) + 1;
-  }
-  return sum;
-}
-
-function pushToArray(words) {
-  let noSpace = words.map((x) => x.replaceAll(" ", ""));
-  let newArr = [];
-  for (let i = 0; i < noSpace.length; i++) {
-    newArr=(noSpace.map((x) => sumUp(x) ));
-  }
-
-  return newArr;
-}
+const film = {
+  title: "Killing of Flower Moon",
+  director: "Martin Scorsese",
+  times: ["15:35"],
+  certificate: "15",
+  duration: 112,
+};
+console.log(film);
+const filmCard=document.createElement("section")
+const title=document.createElement("h1")
+const director=document.createElement("p")
+director.textContent=film.director
+title.textContent=film.title
+filmCard.append(title,director)
+const bodyEl=document.querySelector("body")
+bodyEl.append(filmCard)
