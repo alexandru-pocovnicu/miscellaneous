6a09c3c7849790dfac3e0cbbc150759548f322b8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-24 18:21:00 +0000

    refactor: separate film data into individual constants and update film card creation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const film = {
+const film1 = {
   title: "Killing of Flower Moon",
   director: "Martin Scorsese",
   times: ["15:35"],
@@ -6,9 +6,18 @@ const film = {
   duration: 112,
 };
 
+const film2 = {
+  title: "1984",
+  director: "joe pesci",
+  times: ["15:35"],
+  certificate: "15",
+  duration: 117,
+};
+
+function createFilmCard(film){
 const filmCard = document.getElementById("film-card").content.cloneNode(true);
-
-
 filmCard.querySelector("h3").textContent=film.title
 filmCard.querySelector("p").textContent = film.director;
-document.body.append(filmCard)
+return filmCard
+}
+document.body.append(createFilmCard(film1),createFilmCard(film2))
