142b1fc87308118469d870eba42b1c31085a41f7
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-24 18:37:52 +0000

    refactor: consolidate film data into an array and streamline film card creation

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-const film1 = {
-  title: "Killing of Flower Moon",
-  director: "Martin Scorsese",
-  times: ["15:35"],
-  certificate: "15",
-  duration: 112,
-};
+const films = [
+  {
+    title: "Killing of Flower Moon",
+    director: "Martin Scorsese",
+    times: ["15:35"],
+    certificate: "15",
+    duration: 112,
+  },
 
-const film2 = {
-  title: "1984",
-  director: "joe pesci",
-  times: ["15:35"],
-  certificate: "15",
-  duration: 117,
-};
+  {
+    title: "1984",
+    director: "joe pesci",
+    times: ["15:35"],
+    certificate: "15",
+    duration: 117,
+  },
+];
 
-function createFilmCard(film){
-const filmCard = document.getElementById("film-card").content.cloneNode(true);
-filmCard.querySelector("h3").textContent=film.title
-filmCard.querySelector("p").textContent = film.director;
-return filmCard
+function createFilmCard(film) {
+  const filmCard = document.getElementById("film-card").content.cloneNode(true);
+  
+  filmCard.querySelector("h3").textContent = film.title;
+  filmCard.querySelector("p").textContent = film.director;
+
+  return filmCard;
 }
-document.body.append(createFilmCard(film1),createFilmCard(film2))
+
+for(const film of films){
+  const card=createFilmCard(film)
+  document.body.append(card);
+}
+
+
