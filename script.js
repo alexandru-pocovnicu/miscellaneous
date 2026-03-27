259fac6fd3239e2e477911c93541e4c4921a4ad1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-27 17:48:54 +0000

    You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
    Your function will accept three arguments:
    The first and second argument should be numbers.
    The third argument should represent a sign indicating the operation to perform on these two numbers.
    
    You should return the result of applying the given operation to these numbers.
    
    Note: In dynamically typed languages (JS, PHP, Python), the first and second arguments can be not numbers. In that case, return "unknown value".

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 
-function squaresOnly(a) {
-  let y = [];
-  for (let x of a) {
-    if (Number.isInteger(Math.sqrt(x))) {
-      y.push(x);
-    }
+function calculator(a, b, sign) {
+  let signs=["+","-","*","/"]
+  if(typeof a!=="number"||typeof b!=="number"||!signs.includes(sign)){
+    return "unknown value";
   }
-  return y;
+  if(b===0 && sign==="/"){
+    return Infinity
+  }
+  let result;
+  switch (sign){
+    case "+":
+      result=a+b;
+      break;
+    case "-":
+      result=a-b;
+      break;
+    case "*":
+      result=a*b;
+      break;
+    case "/":
+      result=a/b;
+    
+  }
+return result
 }
+console.log(calculator(6,2,"/"));
