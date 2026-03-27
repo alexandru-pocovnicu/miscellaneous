8e8bb5c33c482f70755969d4eb1cf243c3bde921
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-27 11:07:38 +0000

    Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
    E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-const films = [
-  {
-    title: "Killing of Flower Moon",
-    director: "Martin Scorsese",
-    times: ["15:35"],
-    certificate: "15",
-    duration: 112,
-  },
-
-  {
-    title: "1984",
-    director: "joe pesci",
-    times: ["15:35"],
-    certificate: "15",
-    duration: 117,
-  },
-];
-
-function createFilmCard(film) {
-  const filmCard = document.getElementById("film-card").content.cloneNode(true);
-  
-  filmCard.querySelector("h3").textContent = film.title;
-  filmCard.querySelector("p").textContent = film.director;
-
-  return filmCard;
+function tripleTrouble(one, two, three) {
+  let str=""
+  for(let i=0;i<one.length;i++){
+    str+=one[i]+two[i]+three[i]
+  }
+  return str
 }
-
-for(const film of films){
-  const card=createFilmCard(film)
-  document.body.append(card);
-}
-
 
