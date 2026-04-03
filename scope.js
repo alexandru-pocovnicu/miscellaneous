69de927c2197e0522f75baa0cc487857f62477e1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-03 18:21:03 +0100

    Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
    You need to consider the following ratings:
    
    Terrible: tip 0%
    Poor: tip 5%
    Good: tip 10%
    Great: tip 15%
    Excellent: tip 20%
    The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
    
    "Rating not recognised" in Javascript, Python and Ruby...
    ...or null in Java
    ...or -1 in C#
    Because you're a nice person, you always round up the tip, regardless of the service.

--- scope.js
+++ scope.js
@@ -1,6 +1,16 @@
-const cannonsReady = (gunners) => {
-return Object.values(gunners).every((value) => value === "aye") ? 'Fire!':'Shiver me timbers'
-};
-console.log(
-  cannonsReady({ Mike: "aye", Joe: "aye", Johnson: "aye", Peter: "nay" }),
-);
+function calculateTip(amount, rating) {
+   switch(rating.toLowerCase()){
+    case "terrible":
+        return 0;
+    case "poor":
+        return Math.ceil(amount*(5/100));
+    case "good":
+        return Math.ceil(amount * (10 / 100));
+    case "great":
+        return Math.ceil(amount * (15 / 100));
+    case "excellent":
+        return Math.ceil(amount * (20 / 100));   
+    default:
+        return "Rating not recognised";  
+   } 
+}
