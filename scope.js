d02a9dcc64a2ef25d5f68b15e3bbd5d63b8ada61
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-05 22:30:45 +0100

    This is a beginner friendly kata especially for UFC/MMA fans.
    It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
    
    If the winner is George Saint Pierre he will obviously say:
    
    "I am not impressed by your performance."
    If the winner is Conor McGregor he will most undoubtedly say:
    
    "I'd like to take this chance to apologize.. To absolutely NOBODY!"
    Good Luck!
    
    Note
    The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive).

--- scope.js
+++ scope.js
@@ -1,16 +1,5 @@
-function calculateTip(amount, rating) {
-   switch(rating.toLowerCase()){
-    case "terrible":
-        return 0;
-    case "poor":
-        return Math.ceil(amount*(5/100));
-    case "good":
-        return Math.ceil(amount * (10 / 100));
-    case "great":
-        return Math.ceil(amount * (15 / 100));
-    case "excellent":
-        return Math.ceil(amount * (20 / 100));   
-    default:
-        return "Rating not recognised";  
-   } 
+function quote(fighter) {
+  return fighter.toLowerCase() === "george saint pierre"
+    ? "I am not impressed by your performance."
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
 }
