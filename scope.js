e18afc8a2826684a72ef4005663fa358c229a0dd
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-04-06 09:31:39 +0100

    The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")

--- scope.js
+++ scope.js
@@ -1,5 +1,8 @@
-function quote(fighter) {
-  return fighter.toLowerCase() === "george saint pierre"
-    ? "I am not impressed by your performance."
-    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+function usdcny(usd) {
+  let yuan=usd*6.75
+  if(Number.isInteger(yuan)){
+    return `${yuan}.00 Chinese Yuan`; 
+  }
+  return `${yuan.toFixed(2)} Chinese Yuan` 
 }
+console.log(usdcny(7298));
--- script.js
+++ script.js
@@ -1,27 +1,56 @@
+const state = {
+  films: [
+    {
+      title: "Jurrasic park",
+      director: "bob",
+      times: ["15:00", "20:00"],
+      certificate: "12A",
+      duration: 109,
+    },
+    {
+      title: "friends",
+      director: "bill",
+      times: ["15:00", "20:00"],
+      certificate: "12A",
+      duration: 123,
+    },
+    {
+      title: "lost",
+      director: "jane",
+      times: ["15:00", "20:00"],
+      certificate: "12A",
+      duration: 87,
+    },
+    {
+      title: "south park",
+      director: "eu",
+      times: ["15:00", "20:00"],
+      certificate: "12A",
+      duration: 871,
+    },
+  ],
+  searchTerm: "",
+};
 
-function calculator(a, b, sign) {
-  let signs=["+","-","*","/"]
-  if(typeof a!=="number"||typeof b!=="number"||!signs.includes(sign)){
-    return "unknown value";
-  }
-  if(b===0 && sign==="/"){
-    return Infinity
-  }
-  let result;
-  switch (sign){
-    case "+":
-      result=a+b;
-      break;
-    case "-":
-      result=a-b;
-      break;
-    case "*":
-      result=a*b;
-      break;
-    case "/":
-      result=a/b;
-    
-  }
-return result
+function createFilmCard(film) {
+  const filmCard = document.getElementById("film-card").content.cloneNode(true);
+  filmCard.querySelector("h3").textContent = film.title;
+  filmCard.querySelector("p").textContent = film.director;
+  return filmCard;
 }
-console.log(calculator(6,2,"/"));
+
+function render() {
+  const filteredFilms = state.films.filter((film) =>
+    film.title.includes(state.searchTerm),
+  );
+  const filmCards = filteredFilms.map((eachFilm) => createFilmCard(eachFilm));
+  document.getElementById("film-container").append(...filmCards);
+}
+render();
+
+const input = document.getElementById("q");
+input.addEventListener("keyup", () => {
+  state.searchTerm = input.value;
+  document.getElementById("film-container").innerHTML=""
+  render()
+});
