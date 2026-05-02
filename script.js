db846df34076ac5cef26a75207baeebdbbc7f7cf
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-05-02 17:35:50 +0100

    Implement a function to calculate the distance between two points in n-dimensional space. The two points will be passed to your function as arrays of the same length.
    Round your answers to two decimal places.

--- script.js
+++ script.js
@@ -1,56 +1,8 @@
-const state = {
-  films: [
-    {
-      title: "Jurrasic park",
-      director: "bob",
-      times: ["15:00", "20:00"],
-      certificate: "12A",
-      duration: 109,
-    },
-    {
-      title: "friends",
-      director: "bill",
-      times: ["15:00", "20:00"],
-      certificate: "12A",
-      duration: 123,
-    },
-    {
-      title: "lost",
-      director: "jane",
-      times: ["15:00", "20:00"],
-      certificate: "12A",
-      duration: 87,
-    },
-    {
-      title: "south park",
-      director: "eu",
-      times: ["15:00", "20:00"],
-      certificate: "12A",
-      duration: 871,
-    },
-  ],
-  searchTerm: "",
-};
-
-function createFilmCard(film) {
-  const filmCard = document.getElementById("film-card").content.cloneNode(true);
-  filmCard.querySelector("h3").textContent = film.title;
-  filmCard.querySelector("p").textContent = film.director;
-  return filmCard;
+function euclideanDistance(a, b) {
+  let distance = 0;
+  for (let i = 0; i < a.length; i++) {
+    distance += Math.pow(b[i] - a[i], 2);
+    console.log(distance);
+  }
+  return Number(Math.sqrt(distance).toFixed(2));
 }
-
-function render() {
-  const filteredFilms = state.films.filter((film) =>
-    film.title.includes(state.searchTerm),
-  );
-  const filmCards = filteredFilms.map((eachFilm) => createFilmCard(eachFilm));
-  document.getElementById("film-container").append(...filmCards);
-}
-render();
-
-const input = document.getElementById("q");
-input.addEventListener("keyup", () => {
-  state.searchTerm = input.value;
-  document.getElementById("film-container").innerHTML=""
-  render()
-});
