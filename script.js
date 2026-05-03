7472dde6d2cd115cb704bbb39c08d1147c38acae
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-05-03 09:26:45 +0100

    Refactor solution function for improved readability and consistency

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function solution(str) {
   let strLength = str.length;
-  let arr=[...str]
-  let newArr=[]
-  for(let i=0;i<strLength;i+=2){
-    if(!arr[i+1]){
-      arr[i+1]='_'
+  let arr = [...str];
+  let newArr = [];
+  for (let i = 0; i < strLength; i += 2) {
+    if (!arr[i + 1]) {
+      arr[i + 1] = "_";
     }
-    newArr.push(arr[i].concat(arr[i+1]))
-    
+    newArr.push(arr[i].concat(arr[i + 1]));
   }
-  return newArr
+  return newArr;
 }
-
-console.log(solution("abc"));
